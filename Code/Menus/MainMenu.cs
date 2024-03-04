0d30492f32c44bb9f2de1c06b754ad29110df511
mike <mjschanck@gmail.com>
2024-03-03 23:29:06 -0500

    basic main menu

---
+++ Code/Menus/MainMenu.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void StartButtonPressed()
+	{
+		this.GetTree().ChangeSceneToFile("res://Scenes/CardsMain.tscn");
+	}
+
+	public void QuitButtonPressed()
+	{
+		this.GetTree().Quit();
+	}
+}
