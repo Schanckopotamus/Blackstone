b38099d81227051acc98e178c22955e81bb00958
mike <mjschanck@gmail.com>
2024-03-09 10:31:39 -0500

    Rules and PlayerSelection

---
+++ Code/GameMetadata.cs
@@ -0,0 +1,13 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackstone.Code
+{
+    public partial class GameMetadata : Node
+    {
+    }
+}
